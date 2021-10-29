e953222872fdbed56ea1a13698b24600ee6c2efb
uyarber <86857361+uyarber@users.noreply.github.com>
2021-10-29 19:48:55 -0400

    Update webchat.js

--- Team2_WebChat/webchat.js
+++ Team2_WebChat/webchat.js
@@ -65,6 +65,23 @@ function sendText() {
   });
 }
 
+
+<p>Click on the button below. The input field will tell you when two, four, and six seconds have passed.</p>
+
+<button onclick="timedText()">Display timed text</button>
+<input type="text" id="txt">
+
+<script>
+function timedText() {
+  var x = document.getElementById("txt");
+  setTimeout(function(){ x.value="2 seconds" }, 2000);
+  setTimeout(function(){ x.value="4 seconds" }, 4000);
+  setTimeout(function(){ x.value="6 seconds" }, 6000);
+}
+</script>
+
+
+
 function processResults(data) {
   // add to the bottom of the chat box
   console.log("got:"+data);
